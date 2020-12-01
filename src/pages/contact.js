1c48fdae707f0f1a2e2416033181edf2b4deb566
Zerogami01 <zerogamimusic@gmail.com>
2020-12-02 02:25:32 +0900

    first commit

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>I'd love to talk! Email me at the address below</h1>
+      <p>
+        <a href="zerogamimusic@gmail.com">myaddres@gmail.com</a>
+      </p>
+    </div>
+  )
+}
